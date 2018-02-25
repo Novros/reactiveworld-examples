67c5ac2eeb13296c36718e88ee6ffd9dd9d24b2a
Rostislav Novák <rostislav.novak92@gmail.com>
2018-02-25 13:45:11 +0100

    Added one example of observable functions.

---
+++ src/cz/novros/reactiveworld/ObservableFunctions.java
@@ -0,0 +1,21 @@
+package cz.novros.reactiveworld;
+
+import cz.novros.reactiveworld.observer.PrintObserver;
+import io.reactivex.Observable;
+
+public class ObservableFunctions {
+
+	public static void main(final String[] args) {
+		System.out.println("------------------------------------------");
+		System.out.println("    Example of functions of Observable    ");
+		System.out.println("------------------------------------------");
+		merge();
+	}
+
+	private static void merge() {
+		System.out.println("--- Merge function");
+		final Observable<Integer> observableA = Observable.just(1, 2);
+		final Observable<Integer> observableB = Observable.just(5, 6);
+		observableA.mergeWith(observableB).subscribe(new PrintObserver());
+	}
+}
